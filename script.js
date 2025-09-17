f3f2d10988acd7bda8c7582d08a30f3f21472302
chouhanvikash <96488608+chouhanvikash@users.noreply.github.com>
2025-09-17 09:03:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function volume_sphere() {
     //Write your code here
+	  event.preventDefault();
+let r = parseFloat(document.getElementById("radius").value);
+
+	
+let v=(4/3)*3.14*r*r*r;
+let volume=document.getElementById("volume");
+	volume.value = v;
+	
   
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// window.onload = document.getElementById('MyForm').onsubmit =
+// volume_sphere;
+window.onload = function() {
+  document.getElementById('MyForm').onsubmit = volume_sphere;
+}
+
